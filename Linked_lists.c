76728df384bb865949013eb8daf007334ff791e2
sstock2005 <sstock2005@outlook.com>
2024-01-05 09:40:34 -0600

    linked list complete

--- Linked_lists.c
+++ Linked_lists.c
@@ -39,19 +39,32 @@ int pop(node_t ** head)
     return retval;
 }
 
+/* this actually was hard */
 int remove_by_value(node_t ** head, int val) 
 {
-    /* TODO: remove the found pointer from list */
-    node_t * current = (*head);
-    while (current->val != val)
+    node_t * current = (*head); // create and set current position in node tree
+    node_t * next_node = NULL; // create next node in node tree
+    node_t * prev_node = NULL; // create previous node in node tree
+    int ret = -1; // create and set return value
+    while (ret == -1) // while return value is error / not found
     {
-        printf("Iterating through: %d\n", current->val);
-        current = current->next;
+        if (current->val == val) // if current pointer's value is the desired value
+        {
+            next_node = current->next; // set next_node to the next node in the node tree
+            free(current); // deallocate the current node in node tree
+            current = prev_node; // set current node in node tree to previous node in node tree
+            current->next = next_node; // set next node in node tree to next node in node tree 
+            ret = 0; // return 0, it worked!
+        }
+        else // if current pointer's value is not the desired value
+        {
+            prev_node = current; // set prev_node to the current node in node tree that will then be the previous node in the next iteration
+            current = current->next; // set current node in node tree to the next node in node tree, basically iterating thorugh the tree
+            if (current == NULL){ break; } // if the now current node in node tree is NULL, we hit the end of the node tree and return return value
+            ret = -1; // not necessary, but makes sure that the return is still -1
+        }
     }
-    if (current->val == val)
-    {
-        printf("we found it :)\n");
-    }
+    return ret;
 }
 
 int main() {
@@ -68,5 +81,5 @@ int main() {
 
     remove_by_value(&test_list, 3);
 
-    //print_list(test_list);
+    print_list(test_list);
 }
